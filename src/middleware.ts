c8e3f41b92dbe8ed1aadfa2a2806af7b6726a3a6
James Peralta <japeralta@rippling.com>
2025-06-20 21:41:37 -0700

    Only allow admins to access analytics

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
+const ADMINS = ['jamesperalta35@gmail.com'];
+const EDITORS = ['castleridge.labs@gmail.com'];
+
 export async function middleware(req: NextRequest) {
     try {
         const token = await getToken({ 
@@ -11,7 +14,10 @@ export async function middleware(req: NextRequest) {
         if (!token) {
             return NextResponse.redirect(new URL('/', req.url));
         }
-         
+
+        if (!ADMINS.includes(token.email || '')) {
+            return NextResponse.redirect(new URL('/', req.url));
+        }         
     } catch (error) {
         console.log('error', error);
         return NextResponse.redirect(new URL('/', req.url));
