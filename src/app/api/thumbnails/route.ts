0a5f40186ca92c8fae130a7520c7c13ced16dafa
James Peralta <japeralta@rippling.com>
2025-06-21 01:16:20 -0700

    working!!

--- src/app/api/thumbnails/route.ts
+++ src/app/api/thumbnails/route.ts
@@ -4,12 +4,13 @@ import { createClient } from '@supabase/supabase-js'
 export async function GET(request: Request) {
   try {
     // Get query parameters
-    const supabase = createClient(process.env.SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
     let { data, error } = await supabase
-    .from('thumbnail_jobs')
-    .select('*')
+        .from('thumbnail_jobs')
+        .select('*')
+
     return NextResponse.json({
-        data
+        data,
     });
   } catch (error: unknown) {
     console.error('Error fetching videos:', error);
@@ -22,4 +23,21 @@ export async function GET(request: Request) {
       { status: 500 }
     );
   }
-} 
+}
+
+
+export async function POST(request: Request) {
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+  
+  const body = await request.json()
+  const { data, error } = await supabase.from('thumbnail_jobs').insert({
+    name: body.name,
+    status: body.status,
+    video_link: body.video_link,
+  })
+
+  return NextResponse.json({
+    data,
+    error,
+  })
+}
